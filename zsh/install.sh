16971550a0dae5ac30d6db0d1d311d14ae0a47d7
vincentVermeerschCap <130646539+vincentVermeerschCap@users.noreply.github.com>
2023-07-25 16:13:07 +0200

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,7 @@
 # Config
 mkdir $HOME/.config/zsh
 
+ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
 ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.config/zsh/.zshrc"
 ln -sf "${DOTFILES_LOCATION}/zsh/zsh-aliases" "${HOME}/.config/zsh/zsh-aliases"
 ln -sf "${DOTFILES_LOCATION}/zsh/zsh-exports" "${HOME}/.config/zsh/zsh-exports"
